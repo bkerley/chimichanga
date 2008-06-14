cb398db2acb4811b0430fee5b25b9217dd6161b4
Bryce Kerley <bkerley@brycekerley.net>
2008-06-14 19:15:15 -0400

    canary test code

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "stack.h"
 
 int main() {
+	stack_entry canary;
 	fprintf(stderr, "chimichanga!\n");
+	stack_push('8');
+	canary = stack_pop();
+	fprintf(stderr, "canary %s\n",canary=='8'?"matches":"doesn't match");
 	return 0;
 }
