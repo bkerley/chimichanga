0650d6446561e7e9c81ed4db96cd586a7e5867bf
Bryce Kerley <bkerley@brycekerley.net>
2008-06-14 22:48:16 -0400

    runnable

--- eval.c
+++ eval.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#include "symbol.h"
 #include "eval.h"
 #include "class.h"
 
@@ -8,5 +12,19 @@ void eval(token_array* line) {
 	for (i = 0; i < line->count; i++) {
 		cur_word = line->body[i];
 		cur_class = class_id(cur_word);
+		
+		//the big switch
+		switch (cur_class) {
+			case SYMBOL:
+				stack_push(symbol_intern(cur_word));
+				break;
+			case NUMBER:
+				stack_push((stack_entry)strtol(cur_word, NULL, 0));
+				break;
+			case OPCODE:
+				fprintf(stderr,
+					"Should be executing %s right now (%s:%d)\n", cur_word, __FILE__, __LINE__);
+				break;
+		}
 	}
 }
