9cd5124e97fe60558e1e2b4f659f88eac04a8d64
Bryce Kerley <bkerley@brycekerley.net>
2008-09-27 19:14:40 -0400

    pre2c supports flexible asm creation, kind of useless actually

--- pre2c.rb
+++ pre2c.rb
@@ -3,6 +3,7 @@ outfile = File.open(ARGV[0].gsub(/pre$/,'c'),'w')
 
 names = {}
 outfile.puts '#include "symbol.h"'
+outfile.puts '#define NT "\n\t"'
 
 infile.each_line do |o|
 	
@@ -15,13 +16,34 @@ infile.each_line do |o|
 	
 	if o =~ /binop\((.+)\);$/
 		op = $1
-		o = <<EOF
-	long a,b,r;
-	a = popl();
-	b = popl();
-EOF
+		o = <<-EOF
+			long a,b,r;
+			a = popl();
+			b = popl();
+		EOF
 		o << "\tr = " << op << ";\n"
 		o << "\tpush(r);\n"
+	end
+	
+	if o =~ /aa\((.+)\);$/
+		op = $1
+		o = <<-EOF
+			asm (
+				"xor	%%rax, %%rax" NT
+				"call	_stack_pop" NT
+				"push	%%rax" NT
+				"xor	%%rax, %%rax" NT
+				"call	_stack_pop" NT
+				"pop	%%rbx" NT
+				--OPCODE-- NT
+				"mov	%%rbx, %%rdi" NT
+				"call	_stack_push" NT
+				: /* no output */
+				: /* no input */
+				: "%rax", "%rbx", "%rdi"
+			);
+		EOF
+		o = o.gsub('--OPCODE--', op)
 	end
 	
 	o = o.gsub(/popl\(\)/,"(long)stack_pop()")
